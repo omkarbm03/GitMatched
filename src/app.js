d45f0d69f1cbc7282c7d2c1b269137aaae2f5a02
omkarbm03 <omkarbm03@gmail.com>
2025-07-21 04:23:05 -0400

    Created a Express Server

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express =  require("express");
+
+const app = express();
+
+
+app.use("/hello",(req, res) => {
+    res.send("hello from the server");
+});
+
+app.use("/test",(req, res) => {
+    res.send("hello from the server");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running");
+});
