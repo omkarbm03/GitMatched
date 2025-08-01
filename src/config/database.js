234957fd3f5c081fa5941692845e1316b628a444
omkarbm03 <omkarbm03@gmail.com>
2025-08-01 03:11:17 -0400

    Updated DB

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://omkarbm03:Fy48f6eri5hKNUvC@cluster0.fghthxu.mongodb.net/GitMatched");
+    await mongoose.connect("Your URL");
 }
 
 
