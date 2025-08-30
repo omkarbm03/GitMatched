53a1e74671b415a5c6a40434070b67e1ef93286f
Omkar <147095427+omkarbm03@users.noreply.github.com>
2025-08-30 04:23:30 -0400

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://omkarbm03:Deadpool3@cluster0.fghthxu.mongodb.net/GitMatched"
+    "DB URL"
   );
 };
 
