44cdfe6f24a4dd3c232471a31de9bd9115725fad
omkarbm03 <omkarbm03@gmail.com>
2025-09-04 18:41:22 -0400

    Added .env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,14 @@
+
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
+<<<<<<< HEAD
     "DB URL"
+=======
+    process.env.DB_URL
+>>>>>>> 0eccaa7 (Added .env file)
   );
 };
 
