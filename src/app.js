0c0d6b2ea92b335ee0b6cba1306399f2c2ffaa21
omkarbm03 <omkarbm03@gmail.com>
2025-09-04 23:20:25 -0400

    Added Socket.io for messaging

--- src/app.js
+++ src/app.js
@@ -6,12 +6,12 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const http = require("http");
 const cors = require("cors");
 require("dotenv").config();
 
 const options = {
-  origin: ["http://localhost:5173",
-  "http://127.0.0.1:5173"],
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -25,17 +25,29 @@ app.options(/.*/, cors(options));
 app.use(express.json());
 app.use(cookieParser());
 
-
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 
+const server = http.createServer(app);
+const socket = require("socket.io");
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket)=>{
+
+});
+
 //connect to the database
 connectDB()
   .then(() => {
     console.log("Connected");
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log("Server is running");
     });
   })
